e8cddac293bcf6bec735ba55be472f0c15c2089d
raj22001 <rajmahajan960@gmail.com>
2024-05-23 16:50:11 +0530

    change

--- src/component/DeliveredOrderDetails.jsx
+++ src/component/DeliveredOrderDetails.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import OrderDetailsData from './common/orderDetailsData';
+import OrderDetailsData from './common/OrderDetailsData';
 import getToken from './auth/GetToken';
 
 const DeliveredOrderDetails = () => {
