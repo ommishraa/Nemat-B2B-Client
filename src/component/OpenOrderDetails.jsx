9ac42bcdd1de477026f35e9722aa0cb68902c85f
raj22001 <rajmahajan960@gmail.com>
2024-05-23 17:03:17 +0530

    solve

--- src/component/OpenOrderDetails.jsx
+++ src/component/OpenOrderDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import OrderDetailsData from "./common/orderDetailsData";
+import OrderDetailsData from "./common/OrderDetailsData";
 import getToken from "./auth/GetToken";
 
 
